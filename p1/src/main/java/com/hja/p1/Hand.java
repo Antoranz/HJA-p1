e36067cb989ce5ba7cdb058327194a44785e8161
vCarpan <126760547+vCarpan@users.noreply.github.com>
2023-09-15 17:38:09 +0200

    Proyecto entero

---
+++ p1/src/main/java/com/hja/p1/Hand.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hja.p1;
+
+import java.util.*;
+/**
+ *
+ * @author usuario_local
+ */
+public class Hand {
+    public Set<Card> handSet = new HashSet<Card>();
+    public static final int NCARDS = 5;
+    
+    public Hand(String hand){
+        for(int i=0; i< NCARDS; i=i+2){
+            handSet.add(createCard(hand.charAt(i)+"", hand.charAt(i+1)+""));
+        }
+    }
+    
+    public Card createCard(String number, String suit){
+        return new Card(number, suit); 
+    }
+    
+    public String readHand(){
+        return "pair";
+    }
+}
