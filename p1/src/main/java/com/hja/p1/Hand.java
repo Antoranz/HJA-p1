ea3b4895bd27c9862664b9415cf203ba261713a3
vCarpan <penapancarlos@gmail.com>
2023-09-18 20:31:11 +0200

    Creado clase Pair.java para poder hacer parejas Cambiado el return de readHand() a un Pair<String, Integer> donde String es el valor que tenía anteriormente y el Integer es el valor de la mano que utilizo para la nueva función de comparación. Cambiado también los ejemplos del main.

--- p1/src/main/java/com/hja/p1/Hand.java
+++ p1/src/main/java/com/hja/p1/Hand.java
@@ -3,31 +3,29 @@ package com.hja.p1;
 import java.util.*;
 
 public class Hand implements Comparable<Hand>{
-    
     ArrayList<Card> handList = new ArrayList<>();
     private static final int NCARDS = 5;
-    private final String handValue;
+    private String handString;
+    private int handValue;
     
     public Hand(String hand){
         for(int i=0; i< NCARDS *2; i=i+2){
             handList.add(createCard(hand.charAt(i)+"", hand.charAt(i+1)+""));
         }
         Collections.sort(handList);
-        handValue = readHand();
+        Pair<String, Integer> a = readHand(); 
+        handString = a.getElement0();
+        handValue = a.getElement1();
     }
     
     public Card createCard(String number, String suit){
         return new Card(number, suit); 
     }
     
-    public String getHandValue(){
-        return handValue;
-    }
-    
-    private String readHand(){
+    public Pair<String,Integer> readHand(){
         
         //Se calcula todas las posibilidades de un solo recorrido, se puede hacer más eficiente¿?
-        int pair = 0, three = 0, escaleraReal = handList.get(0).getNumber(); boolean lastPair = false, lastThree = false, straight =true, color = true;
+        int pair = 0, three = 0, poker = 0, escaleraReal = handList.get(0).getNumber(); boolean lastPair = false, lastThree = false, straight =true, color = true;
         for (int i = 1; i < handList.size(); i++) {
             if(color && !handList.get(i).getSuit().equalsIgnoreCase(handList.get(i-1).getSuit())) color = false;
             if(handList.get(i).getNumber() == handList.get(i-1).getNumber()){
@@ -40,12 +38,13 @@ public class Hand implements Comparable<Hand>{
                     lastPair = false;
                     lastThree = true;
                 }else if(lastThree){
-                    return "poker"; //3
+                    poker += 1; 
+                    return new Pair("poker", 8); //3
                 }         
             }
             else {
                 if(straight) {
-                    if(handList.get(i).getNumber()-1 == handList.get(i-1).getNumber() || (handList.get(i).getNumber() == 14 && handList.get(i-1).getNumber()==5)) escaleraReal += handList.get(i).getNumber();
+                    if(handList.get(i).getNumber()-1 == handList.get(i-1).getNumber()) escaleraReal += handList.get(i).getNumber();
                     else straight = false; 
                 }
                 lastPair = false;
@@ -53,32 +52,40 @@ public class Hand implements Comparable<Hand>{
             }    
         }
         if(straight && color){
-            if(escaleraReal == 60) return "royal flush"; //1
-            else return "straight flush"; //2
+            if(escaleraReal == 60) return new Pair("Royal flush", 10); //1
+            else return new Pair("Straight flush", 9); //2
         }
-        else if(pair == 1 && three == 1) return "full"; //4
-        else if(color) return "flush"; //5
-        else if(straight) return "straight"; //6
-        else if(pair == 0 && three == 1) return "three"; //7
-        else if(pair >= 1 && three == 0) return pair + " pair"; //8 y 9
+        else if(pair == 1 && three == 1) return new Pair("Full house", 7); //4
+        else if(color) return new Pair("Flush", 6); //5
+        else if(straight) return new Pair("Straight", 5); //6
+        else if(pair == 0 && three == 1) return new Pair("Three of a kind", 3); //7
+        else if(pair >= 1 && three == 0) return new Pair(pair + " pair", 2); //8 y 9
     
-        return "High Card: "+handList.get(NCARDS-1);//10
+        return new Pair("High Card: "+handList.get(NCARDS-1),1);//10
         
-    }
-    
-    @Override
-    public int compareTo(Hand other) {        
-       //TODO
-       String result = this.getHandValue();
-       String otherResult = other.getHandValue();
-       
-       //if(result == otherResult && )
-        return 0;
-       
     }
     
     @Override
     public String toString(){
        return handList.toString();
+    }
+
+    @Override
+    public int compareTo(Hand o) {
+        if(this.handValue > o.handValue) return 1;
+        else if(this.handValue < o.handValue) return -1;
+        else{
+            int tValue;
+            int oValue;
+            for(int i = NCARDS; i >= 0; i--){
+                tValue = handList.get(i).getNumber();
+                oValue = o.handList.get(i).getNumber();
+                if(tValue != oValue){
+                    if(tValue > oValue)return 1;
+                    else return -1;
+                }
+            }
+            return 0;
+        }
     }
 }
--- p1/src/main/java/com/hja/p1/P1.java
+++ p1/src/main/java/com/hja/p1/P1.java
@@ -10,46 +10,42 @@ public class P1 {
         
         Hand hand = new Hand("AhKhJhQhTh");
         System.out.println(hand);
-        System.out.println(hand.getHandValue());
+        System.out.println(hand.readHand().getElement0());
         
         Hand hand2 = new Hand("Th9h8h7h6h");
         System.out.println(hand2);
-        System.out.println(hand2.getHandValue());
+        System.out.println(hand2.readHand().getElement0());
         
         Hand hand3 = new Hand("AhAdAcAs6h");
         System.out.println(hand3);
-        System.out.println(hand3.getHandValue());
+        System.out.println(hand3.readHand().getElement0());
         
         Hand hand4 = new Hand("ThTdTcKhKs");
         System.out.println(hand4);
-        System.out.println(hand4.getHandValue());
+        System.out.println(hand4.readHand().getElement0());
         
         Hand hand5 = new Hand("2hKh8h7h6h");
         System.out.println(hand5);
-        System.out.println(hand5.getHandValue());
+        System.out.println(hand5.readHand().getElement0());
         
         Hand hand6 = new Hand("Th9d8h7d6h");
         System.out.println(hand6);
-        System.out.println(hand6.getHandValue());
+        System.out.println(hand6.readHand().getElement0());
         
         Hand hand7 = new Hand("AhAcAs7d6h");
         System.out.println(hand7);
-        System.out.println(hand7.getHandValue());
+        System.out.println(hand7.readHand().getElement0());
         
         Hand hand8 = new Hand("AhAc6s7d6h");
         System.out.println(hand8);
-        System.out.println(hand8.getHandValue());
+        System.out.println(hand8.readHand().getElement0());
         
         Hand hand9 = new Hand("AcAh6h7d8h");
         System.out.println(hand9);
-        System.out.println(hand9.getHandValue());
+        System.out.println(hand9.readHand().getElement0());
         
         Hand hand10 = new Hand("Ah9h6h7d8h");
         System.out.println(hand10);
-        System.out.println(hand10.getHandValue());
-        
-        Hand hand11 = new Hand("Ah2h3h4d5h");
-        System.out.println(hand11);
-        System.out.println(hand11.getHandValue());
+        System.out.println(hand10.readHand().getElement0());
     }
 }
