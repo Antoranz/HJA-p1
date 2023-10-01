791a67f37497a4e511633b463cf1136b0a0ce6e5
UsadorDeNombre <victogim@ucm.es>
2023-10-01 12:08:56 +0200

    Draw jaja xd

--- p1/src/main/java/com/hja/p1/P1.java
+++ p1/src/main/java/com/hja/p1/P1.java
@@ -80,5 +80,75 @@ public class P1 {
         frame.setVisible(true);  
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); */
         
+
+        
+
+        //PRUEBAS DRAW 
+
+	/*//OPEN STRAIGHT IZQ
+        Hand hand30 = new Hand("QhJh2hKhAh");
+        System.out.println(hand30);
+        System.out.println(hand30.readDraw());
+        //OPEN STRAIGHT DER
+        Hand hand31 = new Hand("2h4hAh3h8h");
+        System.out.println(hand31);
+        System.out.println(hand31.readDraw());
+        //OPEN STRAIGHT
+        Hand hand32 = new Hand("7h8h9hThAh");
+        System.out.println(hand32);
+        System.out.println(hand32.readDraw());
+        //GUTSHOT
+        Hand hand33 = new Hand("7h9h8hJh2h");
+        System.out.println(hand33);
+        System.out.println(hand33.readDraw());
+        //GUTSHOT OPEN STRAIGHT IZQ
+        Hand hand34 = new Hand("9hQhJhKhAh");
+        System.out.println(hand34);
+        System.out.println(hand34.readDraw());
+        //GUTSHOT OPEN STRAIGHT DER
+        Hand hand35 = new Hand("2hAh6h3h4h");
+        System.out.println(hand35);
+        System.out.println(hand35.readDraw());
+        //COLOR
+        Hand hand36 = new Hand("9h2h8sJhAh");
+        System.out.println(hand36);
+        System.out.println(hand36.readDraw());
+        //FULL PAREJAS
+        Hand hand37 = new Hand("9h9s8h8sAh");
+        System.out.println(hand37);
+        System.out.println(hand37.readDraw());
+        //FULL TRIO
+        Hand hand38 = new Hand("9h9s9cJhAs");
+        System.out.println(hand38);
+        System.out.println(hand38.readDraw());
+        //COLOR OPEN STRAIGHT IZQ
+        Hand hand39 = new Hand("QhJs2hKhAh");
+        System.out.println(hand39);
+        System.out.println(hand39.readDraw());
+        //COLOR OPEN STRAIGHT DER
+        Hand hand40 = new Hand("2h4hAh3s8h");
+        System.out.println(hand40);
+        System.out.println(hand40.readDraw());
+        //COLOR GUTSHOT
+        Hand hand41 = new Hand("7h9h8sJh2h");
+        System.out.println(hand41);
+        System.out.println(hand41.readDraw());
+        //COLOR GUTSHOT OPEN STRAIGHT IZQ
+        Hand hand42 = new Hand("9hQhJhKsAh");
+        System.out.println(hand42);
+        System.out.println(hand42.readDraw());
+        //COLOR GUTSHOT OPEN STRAIGHT DER
+        Hand hand43 = new Hand("2hAh6h3s4h");
+        System.out.println(hand43);
+        System.out.println(hand43.readDraw());
+        
+        Hand hand44 = new Hand("QhAsJhTs9h");
+        System.out.println(hand44);
+        System.out.println(hand44.readDraw());
+        
+        Hand hand45 = new Hand("3hAs4h6s5h");
+        System.out.println(hand45);
+        System.out.println(hand45.readDraw());*/
+        
     }
 }
